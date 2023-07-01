c39e37be26acbf5f4bac49fabf01e31946ea8ca8
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-07-01 11:20:10 +0200

    creato controller Api + lista progetti del database in json

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+
+        $projects = Project::all();
+
+        return response()->json([
+            'succes' => true,
+            'projects' => $projects
+        ]);
+    }
+}
